028aab6e8fb259221fe066b5158f8e8f843fef6f
hwarang.oh(오화랑)/풀스택 <keyein123@gmail.com>
2025-02-17 22:00:58 +0900

    ✨ feat : Post Edit and New Page 구현 완료

--- community/main.js
+++ community/main.js
@@ -120,3 +120,18 @@ function renderComments(comments) {
     commentsList.appendChild(clone);
   });
 }
+
+async function renderEditPost() {
+  const postId = window.location.pathname.split("/")[2];
+  const db = await fetch("/data/db.json").then((res) => res.json());
+  const post = db.posts.find((post) => post.id === Number(postId));
+
+  document.getElementById("postTitle").value = post.title;
+  document.getElementById("postContent").value = post.content;
+  document.getElementById("fileLabel").textContent = post.postImage;
+
+  document.getElementById("postImage").addEventListener("change", function (event) {
+    const fileName = event.target.files[0] ? event.target.files[0].name : "파일을 선택해주세요";
+    document.getElementById("fileLabel").textContent = fileName;
+  });
+}
