45bfdb450962bdf7d4a296eb246bc6044ce1e739
hwarang <keyein123@gmail.com>
2025-03-02 22:04:03 +0900

    ✨ feat : Post Detail 유효성 검사 및 댓글 CRUD 기능 추가

--- community/js/posts.js
+++ community/js/posts.js
@@ -1,15 +1,16 @@
+import { getPosts, getUsers } from './util_database.js';
+
 /**
  * IMP : Rendering Post List Page
  */
 export async function renderPosts() {
-  const postList = document.getElementById("postList");
-  const template = document.getElementById("post-template");
-  const db = await fetch("data/db.json").then((res) => res.json());
-  const posts = db.posts;
-  const users = db.users;
+  const postList = document.getElementById('postList');
+  const template = document.getElementById('post-template');
+  const posts = getPosts();
+  const users = getUsers();
 
-  document.getElementById("createPostButton").addEventListener("click", function () {
-    window.location.href = "/posts/create";
+  document.getElementById('createPostButton').addEventListener('click', function () {
+    window.location.href = '/posts/create';
   });
 
   // * Post Template의 Slot에 데이터를 삽입한다.
@@ -19,12 +20,12 @@ export async function renderPosts() {
 
     clone.querySelector("slot[name='title']").textContent = post.title;
     clone.querySelector("slot[name='date']").textContent = post.date;
-    clone.querySelector("slot[name='author']").textContent = author.name;
+    clone.querySelector("slot[name='author']").textContent = author.nickname;
     clone.querySelector("slot[name='likes']").textContent = post.likes;
     clone.querySelector("slot[name='comments']").textContent = post.comments.length;
     clone.querySelector("slot[name='views']").textContent = post.views;
-    clone.querySelector(".post-profile").src = author.profileImage;
-    clone.querySelector(".post-card").addEventListener("click", function () {
+    clone.querySelector('.post-profile').src = author.profileImage;
+    clone.querySelector('.post-card').addEventListener('click', function () {
       window.location.href = `/posts/${post.id}`;
     });
     postList.appendChild(clone);
--- community/js/util_database.js
+++ community/js/util_database.js
@@ -12,6 +12,75 @@ export function removeDB() {
   localStorage.removeItem(DB_STORAGE_KEY);
 }
 
+// IMP : POST CRUD
+export function getPosts() {
+  const db = getDB();
+  return db.posts;
+}
+
+// ? postID -> Number, post.id -> String => 이러한 이유로 == 연산자 사용
+export function getPost(postId) {
+  const db = getDB();
+  return db.posts.find((post) => post.id == postId);
+}
+
+export function createPost() {}
+
+// ? postID -> Number, post.id -> String => 이러한 이유로 == 연산자 사용
+export function getComments(postId) {
+  const db = getDB();
+  return db.comments.filter((comment) => comment.postId == postId);
+}
+
+export function createComment(postId, commentContent, currentUser) {
+  const db = getDB();
+  const comments = db.comments;
+  const post = db.posts.find((post) => post.id == postId);
+  const newId = comments.length > 0 ? Math.max(...comments.map((c) => c.id)) + 1 : 1;
+  const newComment = {
+    id: newId,
+    postId,
+    authorId: currentUser.id,
+    content: commentContent,
+    date: new Date().toISOString(),
+  };
+  post.comments.push(newId);
+  comments.push(newComment);
+  setDB(db);
+}
+
+export function updateComment(commentId, updatedCommentContent) {
+  const db = getDB();
+  const comments = db.comments;
+  const index = comments.findIndex((comment) => comment.id === commentId);
+  comments[index].content = updatedCommentContent;
+  setDB(db);
+}
+
+export function deleteComment(commentId) {
+  const db = getDB();
+  const postId = db.comments.find((comment) => comment.id === commentId).postId;
+  const post = db.posts.find((post) => post.id == postId);
+  post.comments = post.comments.filter((comment) => comment !== commentId);
+  db.comments = db.comments.filter((comment) => comment.id !== commentId);
+  setDB(db);
+}
+
+// IMP : USER CRUD
+/**
+ * TYPE : GET
+ * @returns
+ */
+export function getUsers() {
+  const db = getDB();
+  return db.users;
+}
+
+/**
+ * TYPE : POST
+ * @param {*} user
+ * @returns
+ */
 export function registerUser(user) {
   const db = getDB();
   const users = db.users;
@@ -34,6 +103,10 @@ export function registerUser(user) {
   return { success: true, message: '회원가입이 완료되었습니다!' };
 }
 
+/**
+ * TYPE : PATCH
+ * @param {*} updatedUser
+ */
 export function updateUser(updatedUser) {
   const db = getDB();
   const users = db.users;
@@ -42,6 +115,10 @@ export function updateUser(updatedUser) {
   setDB(db);
 }
 
+/**
+ * TYPE : DELETE
+ * @param {*} userId
+ */
 export function deleteUser(userId) {
   const db = getDB();
   db.users = db.users.filter((user) => user.id !== userId);
