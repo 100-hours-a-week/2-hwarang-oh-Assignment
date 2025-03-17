a762b5c7cc8ba5c4f58280cea89308af118e826d
hwarang.oh(오화랑)/풀스택 <keyein123@gmail.com>
2025-03-17 16:19:22 +0900

    ✨ feat : Controller와 Service, Repository를 만들어서 User를 가져오는 Logic 구현 & Test

---
+++ backend/src/main/java/com/ktb/community/domain/user/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.ktb.community.domain.user.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ktb.community.domain.user.model.entity.User;
+import com.ktb.community.domain.user.service.UserService;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = userService.getUserById(id);
+        return ResponseEntity.ok(user);
+    }
+
+}
